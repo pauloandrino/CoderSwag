2f0fa1cd32a582b2d0aea3f7749655968fc29393
Paulo Andrino <pauloandrino@yahoo.com.br>
2018-11-15 20:16:37 -0200

    recycle view

--- app/src/main/java/paulo/com/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/paulo/com/coderswag/Controller/MainActivity.kt
@@ -2,25 +2,27 @@ package paulo.com.coderswag.Controller
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.ArrayAdapter
-import android.widget.Toast
+import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
-import paulo.com.coderswag.Model.Category
 import paulo.com.coderswag.R
 import paulo.com.coderswag.Services.DataService
-import paulo.com.coderswag.adapters.CategoryAdapter
+import paulo.com.coderswag.adapters.CategoryRecycleAdapter
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: CategoryAdapter
+    lateinit var adapter: CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.categories)
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
 
         categoryListView.adapter = adapter
+
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
 
     }
 }
--- app/src/main/java/paulo/com/coderswag/adapters/CategoryAdapter.kt
+++ app/src/main/java/paulo/com/coderswag/adapters/CategoryAdapter.kt
@@ -10,10 +10,7 @@ import android.widget.TextView
 import paulo.com.coderswag.Model.Category
 import paulo.com.coderswag.R
 
-class CategoryAdapter(context : Context, categories : List<Category>) : BaseAdapter() {
-
-    val context = context
-    val categories = categories
+class CategoryAdapter(val context: Context, val categories: List<Category>) : BaseAdapter() {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val categoryView: View
