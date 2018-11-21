d9524011605cda489af8e9f7022dae16be7ffbc6
Paulo Andrino <pauloandrino@yahoo.com.br>
2018-11-21 20:53:21 -0200

    lesson 07.

--- app/src/main/java/paulo/com/coderswag/Services/DataService.kt
+++ app/src/main/java/paulo/com/coderswag/Services/DataService.kt
@@ -12,24 +12,36 @@ object DataService {
     )
 
     val hats = listOf(
-        Product("Devslopes Graphic Beanies", "$18", "hat01"),
-        Product("Devslopes Hat Black", "$20", "hat02"),
-        Product("Devslopes Hat White", "$18", "hat03"),
-        Product("Devslopes Hat Snapback", "$22", "hat04")
+        Product("Devslopes Graphic Beanies", "$18", "hat1"),
+        Product("Devslopes Hat Black", "$20", "hat2"),
+        Product("Devslopes Hat White", "$18", "hat3"),
+        Product("Devslopes Hat Snapback", "$22", "hat4")
     )
 
     val hoodies = listOf(
-        Product("Devlopes Hoodie Gray", "$28", "hoodie01"),
-        Product("Devlopes Hoodie Red", "$30", "hoodie02"),
-        Product("Devlopes Gray Hoodie", "$28", "hoodie03"),
-        Product("Devlopes Back Hoodie", "$30", "hoodie04")
+        Product("Devlopes Hoodie Gray", "$28", "hoodie1"),
+        Product("Devlopes Hoodie Red", "$30", "hoodie2"),
+        Product("Devlopes Gray Hoodie", "$28", "hoodie3"),
+        Product("Devlopes Back Hoodie", "$30", "hoodie4")
     )
 
     val shirts = listOf(
-        Product("Devlopes Shirt Black", "$18", "shirt01"),
-        Product("Devlopes Badge Shirt Gray", "$20", "shirt02"),
-        Product("Devlopes Logo Shirt Red", "$22", "shirt03"),
-        Product("Devlopes Hustle", "$22", "shirt04"),
-        Product("Kickflip Studios", "$18", "shirt05")
+        Product("Devlopes Shirt Black", "$18", "shirt1"),
+        Product("Devlopes Badge Shirt Gray", "$20", "shir2"),
+        Product("Devlopes Logo Shirt Red", "$22", "shirt3"),
+        Product("Devlopes Hustle", "$22", "shirt4"),
+        Product("Kickflip Studios", "$18", "shirt5")
     )
+
+    val digitalGood = listOf<Product>()
+
+    fun getProducts(category: String) : List<Product> {
+
+        return when(category) {
+            "SHIRTS" -> shirts
+            "HATS" -> hats
+            "HOODIES" -> hoodies
+            else -> digitalGood
+        }
+    }
 }
